f211f5ca694282287fe9fd72ce1ccde21d82b830
Ângelo Hideki Noda <angelo.noda@insidesistemas.com.br>
2022-06-06 17:51:46 -0300

    First Commit

---
+++ CalculadoraSimples.cs
@@ -0,0 +1,23 @@
+﻿namespace NUnitCalculadora
+{
+    public class CalculadoraSimples
+    {
+        /* Método para Adicionar 2 números */
+        public int Adicionar(int numero1, int numero2)
+        {
+            return (numero1 + numero2);
+        }
+        public int Subtrair(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+        public int Multiplicar(int numero1, int numero2)
+        {
+            return (numero1 * numero2);
+        }
+        public double Dividir(double numero1, double numero2)
+        {
+            return (numero1 / numero2);
+        }
+    }
+}
